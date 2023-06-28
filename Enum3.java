cf87c1ec714ebe66bae466b8541afd5fea2da44d
deeptha28 <71130912+deeptha28@users.noreply.github.com>
2023-06-28 16:24:22 +0530

    Add files via upload

---
+++ Enum3.java
@@ -0,0 +1,21 @@
+public class Enum3 {
+    public enum months {
+        JANUARY,
+        FEBRUARY,
+        MARCH,
+        APRIL,
+        MAY,
+        JUNE,
+        JULY,
+        AUGUST,
+        SEPTEMBER,
+        OCTOBER,
+        NOVEMBER,
+        DECEMBER
+        
+    }
+    public static void main(String[] args){
+        months birthday = months.OCTOBER;
+        System.out.println("Birthday month: "+birthday);
+    }
+}
