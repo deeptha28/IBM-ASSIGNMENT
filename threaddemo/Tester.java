778fbcb30ef4710befc4cd4052e25b6deb895e74
deeptha28 <71130912+deeptha28@users.noreply.github.com>
2023-07-04 12:24:27 +0530

    Delete threaddemo directory

--- threaddemo/Tester.java
+++
@@ -1,9 +0,0 @@
-public class Tester {
-	public static void main(String[] args) {
-		TestStack s = new TestStack(); //Stack
-		Thread t1 = new Thread(s);
-		t1.start();
-		Thread t2 = new Thread(s);
-		t2.start();
-	}
-}
--- threaddemo/Tester1.java
+++
@@ -1,9 +0,0 @@
-public class Tester {
-	public static void main(String[] args) {
-	MyQueue myQueue = new MyQueue(); //Queue
-        Thread t3 = new Thread(new TestQueue(myQueue));
-        t3.start();
-        Thread t4 = new Thread(new TestQueue(myQueue));
-        t4.start();
-	}
-}
