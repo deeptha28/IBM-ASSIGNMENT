9080d161c548506316877e57f214d076ff4afddc
deeptha28 <71130912+deeptha28@users.noreply.github.com>
2023-07-04 17:23:25 +0530

    Add files via upload

---
+++ customer_project/AppTest.java
@@ -0,0 +1,17 @@
+package com.ibm.customer_project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
