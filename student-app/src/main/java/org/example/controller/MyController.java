2701e3bbdf99779a9d6fc27441d0c4005dc05d91
deeptha28 <71130912+deeptha28@users.noreply.github.com>
2023-07-13 16:58:53 +0530

    Add files via upload

---
+++ student-app/src/main/java/org/example/controller/MyController.java
@@ -0,0 +1,39 @@
+package org.example.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.example.model.Student;
+
+
+public class MyController extends HttpServlet {
+	
+       
+   
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+	
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		String name=request.getParameter("t1");
+		String grade=request.getParameter("t2");
+		Student student=new Student(name, grade);
+		
+		out.println("hello "+student.getName()+" your grade is: "+student.getGrade());
+		
+	}
+
+}
